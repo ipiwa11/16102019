f6f530d7cb0da9cb063acf1037fe8145d9d05c76
Irina <korablewaira@yandex.ru>
2019-10-16 22:29:24 +0500

    Commit_4

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,15 +2,22 @@
 
 using namespace std;
 
-int main()
-{
-   cout << "Hello world!" << endl;
-   
-   int a;
-   
-   cin >> a;
-   
-   cout << a << endl;
-   
-   return 0;
+int main(){
+   cout<<"Hello world"<<endl;
+char a[100];
+bool IsBreak=false;
+cout<<"¬ведите"<<endl;
+cin>>a;
+for (int i=0; a[i]!='\0'; i++)
+if (a[i]<48 || a[i]>57) {
+cout<<"¬ведено не число\n";
+IsBreak=true;
+break;
+}
+
+if(IsBreak==false){
+   int b = atoi(a);
+cout<<b<<endl;
+}
+return 0;
 }
